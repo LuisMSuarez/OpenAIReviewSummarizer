5e23fc0362a15b9ee2f738f5f6728c7af60ef915
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-09-10 10:37:54 -0700

    Initialized express api

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from 'express'
+import type {Request, Response} from 'express'
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", ( req: Request, res: Response) => {
+    res.send("Hellos World!")
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
