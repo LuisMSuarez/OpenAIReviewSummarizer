9d770e68850f9bdaff63c1cde754ef652194e5c6
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-09-11 14:15:17 -0700

    Controller refactor

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import z from 'zod';
-import { chatService } from './services/chat.service';
+import { chatController } from './controllers/chat.controller';
 
 dotenv.config();
 
@@ -10,39 +9,11 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-   res.send(process.env.OPENAI_API_KEY);
+app.get('/api/ping', (req: Request, res: Response) => {
+   res.json({ message: 'Success!' });
 });
 
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello world!' });
-});
-
-let lastResponseId: string | null = null;
-
-const chatSchema = z.object({
-   prompt: z
-      .string()
-      .trim()
-      .min(1, 'Prompt is required.')
-      .max(1000, 'Prompt is too long.'),
-   conversationId: z.uuid(),
-});
-
-app.post('/api/chat', async (req: Request, res: Response) => {
-   const parseResult = chatSchema.safeParse(req.body);
-   if (!parseResult.success) {
-      res.status(400).json(z.treeifyError(parseResult.error));
-   }
-
-   try {
-      const { prompt, conversationId } = req.body;
-      const response = await chatService.sendMessage(prompt, conversationId);
-      res.json({ message: response.message });
-   } catch (error) {
-      res.status(500).json({ error: 'Failed to generate a response.' });
-   }
-});
+app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
