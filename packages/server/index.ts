d7cd36c52996cda51589d34e10e25af9789c8852
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-09-10 22:35:08 -0700

    fixing route

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -6,7 +6,7 @@ import OpenAI from 'openai';
 dotenv.config();
 
 const client = new OpenAI({
-   apiKey: process.env.OPENAI_API_KEY
+   apiKey: process.env.OPENAI_API_KEY,
 });
 
 const app = express();
@@ -21,18 +21,18 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello world!' });
 });
 
-app.post('api/chat', async (req: Request, res: Response) => {
+app.post('/api/chat', async (req: Request, res: Response) => {
    const { prompt } = req.body;
 
-    const response = await client.responses.create({
+   const response = await client.responses.create({
       model: 'gpt-4o-mini',
       input: prompt,
       temperature: 0.2,
-      max_output_tokens: 100
-   })
+      max_output_tokens: 100,
+   });
 
-   res.json( { message: response.output_text})
-})
+   res.json({ message: response.output_text });
+});
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
