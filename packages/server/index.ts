9bce87b795b4711b3fdb91f6fb0194e29db2fc08
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-09-11 13:35:01 -0700

    Service layer

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,15 +1,10 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
 import z from 'zod';
-import { conversationRespository } from './repositories/conversation.repository';
+import { chatService } from './services/chat.service';
 
 dotenv.config();
-
-const openAiClient = new OpenAI({
-   apiKey: process.env.OPENAI_API_KEY,
-});
 
 const app = express();
 app.use(express.json());
@@ -42,18 +37,8 @@ app.post('/api/chat', async (req: Request, res: Response) => {
 
    try {
       const { prompt, conversationId } = req.body;
-      const response = await openAiClient.responses.create({
-         model: 'gpt-4o-mini',
-         input: prompt,
-         temperature: 0.2,
-         max_output_tokens: 100,
-         previous_response_id:
-            conversationRespository.getLastResponseId(conversationId),
-      });
-
-      conversationRespository.setLastResponseId(conversationId, response.id);
-
-      res.json({ message: response.output_text });
+      const response = await chatService.sendMessage(prompt, conversationId);
+      res.json({ message: response.message });
    } catch (error) {
       res.status(500).json({ error: 'Failed to generate a response.' });
    }
