21e469f0233041268641ec2073c7f4d78296fedb
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-09-30 13:04:46 -0700

    Store review summary in the DB

--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -7,7 +7,10 @@ import { llmProvider, type LlmProvider } from '../providers/llm.provider';
 import template from '../prompts/summarizeReview.txt';
 
 class ReviewService {
-   constructor(private readonly llmProvider: LlmProvider) {}
+   constructor(
+      private readonly llmProvider: LlmProvider,
+      private readonly reviewsRepository: ReviewsRepository
+   ) {}
 
    async getReviews(productId: number): Promise<Review[]> {
       return await reviewsRepository.getReviews(productId);
@@ -18,14 +21,13 @@ class ReviewService {
       var reviews = await reviewsRepository.getReviews(productId, 10);
       const joinedReviews = reviews.map((r) => r.content).join('\n\n');
       const prompt = template.replace('{{ reviews }}', joinedReviews);
-
-      return (
-         await this.llmProvider.generateResponse({
-            prompt,
-            maxOutputTokens: 500,
-         })
-      ).message;
+      const { message: summary } = await this.llmProvider.generateResponse({
+         prompt,
+         maxOutputTokens: 500,
+      });
+      await this.reviewsRepository.storeReviewSummary(productId, summary);
+      return summary;
    }
 }
 
-export const reviewService = new ReviewService(llmProvider);
+export const reviewService = new ReviewService(llmProvider, reviewsRepository);
