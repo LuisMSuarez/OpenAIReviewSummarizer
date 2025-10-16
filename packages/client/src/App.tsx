62bda6abb9f4bd00efb0906034c8a845a1a0fa5c
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-10-16 10:54:41 -0700

    Displaying review list

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
-import ChatBot from './components/ChatBot';
+// import ChatBot from './components/ChatBot';
+import ReviewList from './components/reviews/ReviewList';
 
 function App() {
    // h-[100dvh] works like vh, but it takes into account the url bar that shows/hides on mobile browsers when you scroll.
    return (
       <div className="p-4 w-screen h-[100dvh]">
-         <ChatBot />
+         <ReviewList productId={1} />
       </div>
    );
 }
