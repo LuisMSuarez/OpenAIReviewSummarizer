3a31e244aeb430b36d490ea146bcc64865e335c9
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-10-21 16:43:51 -0700

    react-loading-skeleton

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -21,16 +21,22 @@ type GetReviewsResponse = {
 
 const ReviewList = ({ productId }: Props) => {
    const [reviewData, setReviewData] = useState<GetReviewsResponse>();
+   const [isLoading, setIsLoading] = useState(false);
    const fetchReviews = async ({ productId }: Props) => {
+      setIsLoading(true);
       const { data } = await axios.get<GetReviewsResponse>(
          `/api/products/${productId}/reviews`
       );
       setReviewData(data);
+      setIsLoading(false);
    };
    useEffect(() => {
       fetchReviews({ productId });
    }, []);
 
+   if (isLoading) {
+      return <p>Loading...</p>;
+   }
    return (
       <div className="flex flex-col gap-5">
          {reviewData?.reviews.map((review) => (
